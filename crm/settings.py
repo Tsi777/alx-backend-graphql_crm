157b745fd66439b7331fb2f1c1369d5add0b7d61
Tsi777 <tsionphilipos6@gmail.com>
2025-08-31 11:47:13 +0300

    settings.py

---
+++ crm/settings.py
@@ -0,0 +1,27 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'django_filters',
+    'crm',
+    'django_crontab',
+    'django_celery_beat',
+
+]
+
+CRONJOBS = [
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
+]
+
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
